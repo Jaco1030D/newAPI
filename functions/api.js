6945aecaff332112a63731a484fc225f7e220be8
Jaco Antonio <jaco42818@gmail.com>
2023-10-07 21:33:06 -0300

    teste

--- functions/api.js
+++ functions/api.js
@@ -14,6 +14,8 @@ app.use(cors());
 const port = 5000;
 const projectId = 'eighth-effect-259620';
 
+process.env.GOOGLE_APPLICATION_CREDENTIALS = path.join(__dirname, 'c.json');
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
