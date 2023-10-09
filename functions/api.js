4c8dd63e15bc5ce50161e23f69a933c4fe004b58
Jaco Antonio <jaco42818@gmail.com>
2023-10-08 21:07:07 -0300

    teste

--- functions/api.js
+++ functions/api.js
@@ -17,12 +17,39 @@ const projectId = 'eighth-effect-259620';
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+process.env.GOOGLE_APPLICATION_CREDENTIALS = path.resolve(__dirname, 'c.json');
+
 router.post('/translate', upload.single('file'), async (req, res) => {
-    res.json({
-        hello: "hi!"
-      });
+  try {
+    const translationClient = new TranslationServiceClient();
+
+    const targetLanguage = 'en'; // Defina o idioma de destino desejado
+
+    const documentInputConfig = {
+      content: req.file.buffer, // Usamos o buffer do arquivo enviado
+      mimeType: 'application/pdf',
+    };
+
+    const request = {
+      parent: `projects/${projectId}/locations/us-central1`,
+      documentInputConfig,
+      targetLanguageCode: targetLanguage,
+    };
+
+    const [response] = await translationClient.translateDocument(request);
+
+    res.setHeader('Content-Disposition', 'attachment; filename=seu_arquivo.pdf');
+    res.setHeader('Content-Type', 'application/pdf');
+
+    res.end(response.documentTranslation.byteStreamOutputs[0])
+
+  } catch (err) {
+    console.error('Erro ao traduzir:', err);
+    res.status(500).send('Erro ao traduzir o arquivo.');
+  }
 
 });
+
 app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
