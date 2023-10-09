a48180a9a8c270094ca0504e0bd9b4c878d34c5f
Jaco Antonio <jaco42818@gmail.com>
2023-10-08 21:00:42 -0300

    teste

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { TranslationServiceClient } = require('@google-cloud/translate').v3beta1;
 const serverless = require('serverless-http')
-const meuArquivo = require('./c.json');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
@@ -10,10 +9,6 @@ const router = express.Router()
 const app = express();
 require('dotenv').config();
 app.use(cors())
-const os = require('os');
-
-// Definindo a variável de ambiente GOOGLE_APPLICATION_CREDENTIALS
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path.join(__dirname, 'c.json');
 
 
 const port = 5000;
@@ -23,40 +18,10 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router.post('/translate', upload.single('file'), async (req, res) => {
-  try {
-    const translationClient = new TranslationServiceClient();
+    res.json({
+        hello: "hi!"
+      });
 
-    const targetLanguage = 'en'; // Defina o idioma de destino desejado
-
-    const documentInputConfig = {
-      content: req.file.buffer, // Usamos o buffer do arquivo enviado
-      mimeType: 'application/pdf',
-    };
-
-    const request = {
-      parent: `projects/${projectId}/locations/us-central1`,
-      documentInputConfig,
-      targetLanguageCode: targetLanguage,
-    };
-
-    const [response] = await translationClient.translateDocument(request);
-
-    res.setHeader('Content-Disposition', 'attachment; filename=seu_arquivo.pdf');
-    res.setHeader('Content-Type', 'application/pdf');
-
-    res.end(response.documentTranslation.byteStreamOutputs[0])
-
-  } catch (err) {
-    console.error('Erro ao traduzir:', err);
-    res.status(500).send('Erro ao traduzir o arquivo.');
-  }
-
-});
-
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
 });
 app.use(`/.netlify/functions/api`, router);
 
